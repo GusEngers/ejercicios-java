db9c80ad934462b9a9610043e1fd49c744f5f188
GusEngers <gustavoengers6441@gmail.com>
2023-11-25 19:48:34 -0300

    Ejercicios de numeros: verificar si un numero es positivo o negativo

--- packages/numbers/Number.java
+++ packages/numbers/Number.java
@@ -2,32 +2,53 @@ package numbers;
 import java.util.Scanner;
 
 public class Number {
+	Scanner scanner = new Scanner(System.in);
 	double number1, number2;
+
   public static void main(String[] args) {
   	Number number = new Number();
 
   	// Ejercicio 1:
-  	number.compareNumbers();
+  	// number.compareNumbers();
+
+  	// Ejercicio 2:
+  	number.isPositiveOrNegative();
   }
-  
+
   /**
    * Ejercicio 1: Desarrollar un programa para
    * comparar si dos números ingresados por el
    * usuario son iguales. 
    */
   public void compareNumbers() {
-  	Scanner scanner = new Scanner(System.in);
-
   	System.out.print("Enter the first number: ");
-  	this.number1 = scanner.nextDouble();
+  	this.number1 = this.scanner.nextDouble();
 
   	System.out.print("Enter the second number: ");
-  	this.number2 = scanner.nextDouble();
+  	this.number2 = this.scanner.nextDouble();
 
   	if (this.number1 == this.number2) {
   		System.out.print("The numbers are equal");
   	} else {
   		System.out.print("The numbers aren't equal");
+  	}
+  }
+
+  /**
+   * Ejercicio 2: Desarrollar un programa para
+   * determinar si un número ingresado por el
+   * usuario es positivo o negativo.
+   */
+  public void isPositiveOrNegative() {
+  	System.out.print("Enter a number: ");
+  	this.number1 = this.scanner.nextDouble();
+
+  	if (this.number1 > 0) {
+  		System.out.print("The number is POSITIVE");
+  	} else if (this.number1 < 0) {
+  		System.out.print("The number is NEGATIVE");
+  	} else {
+  		System.out.print("The number is ZERO");
   	}
   }
 }
