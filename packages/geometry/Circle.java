b66fffa6dbd967877f491636427890cfb4b00ab9
GusEngers <gustavoengers6441@gmail.com>
2023-11-25 18:58:17 -0300

    Ejercicios de geometría: perimetro de circulo

--- packages/geometry/Circle.java
+++ packages/geometry/Circle.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 
 public class Circle {
 	public static void main(String[] args) {
+		Circle ejercicio = new Circle();
+
 		// Ejercicio 1:
-		Circle ejercicio1 = new Circle();
-		ejercicio1.getArea();
+		// ejercicio.getArea();
+
+		// Ejercicio 2:
+		ejercicio.getPerimeter();
 	}
 
 	/**
 	 * Ejercicio 1: Desarrollar un programa para obtener el
 	 * área de un círculo según el radio indicado por el
-	 * usuario
+	 * usuario.
+	 * Fórmula:  AREA = PI x RADIO^2
 	 */
 	double radius;
 	public void getArea() {
@@ -21,5 +26,21 @@ public class Circle {
 
 		double area = Math.PI * Math.pow(this.radius, 2);
 		System.out.print("The area of the circle is: " + area);
+	}
+
+
+	/**
+	 *	Ejercicio 2: Desarrollar un programa para obtener el
+	 * perímetro de un círculo según el radio indicado por el
+	 * usuario.
+	 * Fórmula: PERIMETRO = DIAMETRO x PI
+	 */
+	public void getPerimeter() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the radius: ");
+		this.radius = scanner.nextDouble();
+
+		double perimeter = 2 * this.radius * Math.PI;
+		System.out.print("The perimeter of the circle is: " + perimeter);
 	}
 }
